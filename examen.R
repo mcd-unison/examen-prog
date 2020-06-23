61eb89ad7b08d297de356fe532d163645f59ace4
GreciaMendoza <gremav94@gmail.com>
2020-06-23 16:45:49 -0700

    mi primer commit

--- examen.R
+++ examen.R
@@ -1,24 +1,11 @@
-library(epiR)
-library(survival)
+
 library(ggplot2)
 library(data.table)
-names(iris) 
-head(iris, n = 4)
-summary(iris)
-str(iris)
-View(iris)
-write.csv(iris, file = "iris.csv")
 
 setwd("C:/Users/Laptop/Desktop/examen-prog-master/covid-data")
-Covidata <- fread("COVID19MEXICO.csv",select=c("FECHA_ACTUALIZACION","FECHA_INGRESO")
-head(Covidata)
 ep<-read.csv("COVID19MEXICO.csv")                 
 head(ep)
-write.csv(Covidata, file = "Covidata.csv")
-
-
-
-Datos=data.frame(ep$FECHA_ACTUALIZACION,ep$SEXO[])
+Datos=data.frame(ep$FECHA_ACTUALIZACION,ep$FECHA_INGRESO)
 head(Datos)
 View(Datos)
 write.csv(Datos, file="Tabla 1")
