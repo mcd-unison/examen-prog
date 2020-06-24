bf6f06fd8086510a6e4eae53036a56609f958e3b
GreciaMendoza <gremav94@gmail.com>
2020-06-23 19:45:42 -0700

    tabla 1

--- examen.R
+++ examen.R
@@ -1,11 +1,36 @@
-
+#Grecia Mendoza 
 library(ggplot2)
 library(data.table)
+library(dplyr)
 
-setwd("C:/Users/Laptop/Desktop/examen-prog-master/covid-data")
-ep<-read.csv("COVID19MEXICO.csv")                 
-head(ep)
-Datos=data.frame(ep$FECHA_ACTUALIZACION,ep$FECHA_INGRESO)
-head(Datos)
-View(Datos)
-write.csv(Datos, file="Tabla 1")
+#Dirección de trabajo
+setwd("C:/Users/Laptop/Desktop/examen-maestria/examen-prog/covid-data")
+
+#Leemos nuestra base de datos covid
+covid<-read.csv("200511COVID19MEXICO.csv")
+
+#Tabla 1
+
+#Creamos un subconjunto para seleccionar personas únicamente del estado de Sonora y positivos 
+covid1<- subset(covid, ENTIDAD_RES == 26 & RESULTADO==1)
+
+#Creamos un data frame con nuestras variables de interés
+Datos<-data.frame(covid1$FECHA_SINTOMAS,covid1$RESULTADO,covid1$FECHA_DEF)
+
+#Función que nos permite guardar nuestra data frame en un archivo csv
+write.csv(Datos, file="tabla1.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
