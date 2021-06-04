3b98470760dd5f16f9b77d7e34f1a9a0cfc16ae6
ibsan <ibsan_89@hotmail.com>
2021-06-04 16:34:58 -0700

    creacion de tabla tabla2.csv

--- proyecto_final.py
+++ proyecto_final.py
@@ -9,7 +9,11 @@ entidad = 26;
 resultadoCovid = 1
 globaldir= './csv/'
 nameFileSonoraConf = globaldir+'tabla1.csv'
-#PRIMERA PARTE
+
+nameFileSonChPbNlConf = globaldir+'tabla2.csv'
+entidadesHosp= [26,19,8,21]
+tipoPaciente = 2
+#----PRIMERA PARTE----
 #Filtrado de datos
 confirmadosSonora =  pd.DataFrame(newDataSet[(newDataSet['ENTIDAD_RES'] == entidad) & (newDataSet['RESULTADO'] == resultadoCovid)])
 #Creacion de nuevo campo Defuncion
@@ -20,7 +24,31 @@ sonoraConfirmados = pd.DataFrame(confirmadosSonora[['FECHA_SINTOMAS','RESULTADO'
 sonoraAgrupados = sonoraConfirmados.groupby('FECHA_SINTOMAS').sum('DEFUNCION','RESULTADO').reset_index()
 #Ordenar por fecha de sintomas
 ordenCasosMexico = sonoraAgrupados.sort_values('FECHA_SINTOMAS')
+#Cambio de nombre columnas
+hospSonChNLPuAgrupados.columns = ['Fecha Sintomas','Casos','Defunciones']
 #Grabar tabla.csv
 ordenCasosMexico.to_csv(nameFileSonoraConf,index=False)
 
+#----SEGUNDA PARTE FECHA_INGRESO----
+#Filtro por entidades Sonora, Puebla, Nuevo leon, Chihuahua
+hospSonChNLPu = pd.DataFrame(newDataSet[ (
+    (newDataSet['ENTIDAD_RES'] == entidadesHosp[0]) |
+    (newDataSet['ENTIDAD_RES'] == entidadesHosp[1]) |
+    (newDataSet['ENTIDAD_RES'] == entidadesHosp[2]) |
+    (newDataSet['ENTIDAD_RES'] == entidadesHosp[3]) ) & 
+    (
+    (newDataSet['FECHA_INGRESO'] != '9999-99-99') & (newDataSet['FECHA_INGRESO'] != '') 
+    ) &
+    (newDataSet['TIPO_PACIENTE'] == tipoPaciente)
+    ])
+#Crear frame de Entidad y Tipo
+hospEntidades = pd.DataFrame(hospSonChNLPu[['ENTIDAD_RES','TIPO_PACIENTE']])
+#Contar por estado 
+hospSonChNLPuAgrupados = hospEntidades.groupby('ENTIDAD_RES')['TIPO_PACIENTE'].count().reset_index()
+#Remplazando valores por nombre de estado
+hospSonChNLPuAgrupados["ENTIDAD_RES"].replace({26: "Sonora", 21: "Puebla",19:"Nuevo Leon", 8:"Chihuahua"}, inplace=True)
+#cambio de nombre de columnas
+hospSonChNLPuAgrupados.columns = ['Estados','Hospitalizados']
+#Crear csv
+hospSonChNLPuAgrupados.to_csv(nameFileSonChPbNlConf,index=False)
 
